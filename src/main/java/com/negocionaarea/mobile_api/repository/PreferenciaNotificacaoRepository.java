d54c774894a8ecc3a92c9892ccb06622c9efe1e2
Taynad <dtayna3@gmail.com>
2026-04-19 17:34:37 -0300

    feat: criação query calculo distancia

--- src/main/java/com/negocionaarea/mobile_api/repository/PreferenciaNotificacaoRepository.java
+++ src/main/java/com/negocionaarea/mobile_api/repository/PreferenciaNotificacaoRepository.java
@@ -9,5 +9,34 @@ import java.util.List;
 import java.util.UUID;
 
 public interface PreferenciaNotificacaoRepository extends JpaRepository<PreferenciaNotificacaoModel, UUID> {
+    @Query(value = """
+    SELECT DISTINCT pn.*
+    FROM PREFERENCIA_NOTIFICACAO pn
+    JOIN CLIENTES c ON pn.CLIENTE_ID = c.ID
+    JOIN PREFERENCIA_CATEGORIAS pc ON pc.PREFERENCIA_ID = pn.ID
+    JOIN EMPRESAS e ON e.ID = :empresaId
+    WHERE 
+        (
+            pc.CATEGORIA = :categoria
+            OR pn.RECEBER_QUALQUER_PROMO = true
+        )
+    AND c.LATITUDE IS NOT NULL
+    AND c.LONGITUDE IS NOT NULL
+    AND e.LATITUDE IS NOT NULL
+    AND e.LONGITUDE IS NOT NULL
+    AND (
+        6371 * acos(
+            cos(radians(e.LATITUDE)) *
+            cos(radians(c.LATITUDE)) *
+            cos(radians(c.LONGITUDE) - radians(e.LONGITUDE)) +
+            sin(radians(e.LATITUDE)) *
+            sin(radians(c.LATITUDE))
+        )
+    ) <= pn.RAIO_MAXIMO_KM
+    """, nativeQuery = true)
+    List<PreferenciaNotificacaoModel> buscarUsuariosParaNotificacao(
+            @Param("categoria") String categoria,
+            @Param("empresaId") UUID empresaId
+    );
 
 }
