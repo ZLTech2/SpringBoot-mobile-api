75431536ee5eeed6f4854eb05553c1a9c3078dc1
Taynad <166086044+Taynad@users.noreply.github.com>
2026-04-21 14:03:51 -0300

    fix: correção de erro ao transformar endereco em latitude e longitude com a API

--- src/main/java/com/negocionaarea/mobile_api/model/LocalizacaoModel.java
+++ src/main/java/com/negocionaarea/mobile_api/model/LocalizacaoModel.java
@@ -1,5 +1,6 @@
 package com.negocionaarea.mobile_api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
