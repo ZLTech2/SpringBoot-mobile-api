41f8fdf55ee3eaa4e347b110c81fd353ccf4ff33
Taynad <166086044+Taynad@users.noreply.github.com>
2026-04-21 10:44:04 -0300

    feat: criando controller de preferencia notificacao

--- src/main/java/com/negocionaarea/mobile_api/controller/PreferenciaNotificacaoController.java
+++ src/main/java/com/negocionaarea/mobile_api/controller/PreferenciaNotificacaoController.java
@@ -4,6 +4,7 @@ import com.negocionaarea.mobile_api.dto.PreferenciaNotificacaoRequest;
 import com.negocionaarea.mobile_api.model.PreferenciaNotificacaoModel;
 import com.negocionaarea.mobile_api.service.PreferenciaNotificacaoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PreferenciaNotificacaoController {
         this.service = service;
     }
 
+    @PostMapping("/configuracao")
     public ResponseEntity<PreferenciaNotificacaoModel> criar(@RequestBody PreferenciaNotificacaoRequest request){
         return ResponseEntity.ok(service.salvar(request));
     }
