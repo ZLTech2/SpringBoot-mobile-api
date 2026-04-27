096b011b1ea378e898d14e1e0ad1777ef6f898f6
ZL Tech <zltech64@gmail.com>
2026-04-27 00:15:08 -0300

    fix: corrige tabelas

--- src/main/java/com/negocionaarea/mobile_api/repository/PreferenciaNotificacaoRepository.java
+++ src/main/java/com/negocionaarea/mobile_api/repository/PreferenciaNotificacaoRepository.java
@@ -12,28 +12,28 @@ import java.util.UUID;
 public interface PreferenciaNotificacaoRepository extends JpaRepository<PreferenciaNotificacaoModel, UUID> {
     @Query(value = """
     SELECT DISTINCT pn.*
-    FROM PREFERENCIA_NOTIFICACAO pn
-    JOIN CLIENTES c ON pn.CLIENTE_ID = c.ID
-    LEFT JOIN PREFERENCIA_CATEGORIAS pc ON pc.PREFERENCIA_ID = pn.ID
-    JOIN EMPRESAS e ON e.ID = :empresaId
+    FROM preferencia_notificacao pn
+    JOIN clientes c ON pn.cliente_id = c.id
+    LEFT JOIN preferencia_categorias pc ON pc.preferencia_id = pn.id
+    JOIN empresas e ON e.id = :empresaId
     WHERE 
         (
-            pc.CATEGORIA = :categoria
-            OR pn.RECEBER_QUALQUER_PROMO = true
+            pc.categoria = :categoria
+            OR pn.receber_qualquer_promo = true
         )
-    AND c.LATITUDE IS NOT NULL
-    AND c.LONGITUDE IS NOT NULL
-    AND e.LATITUDE IS NOT NULL
-    AND e.LONGITUDE IS NOT NULL
+    AND c.latitude IS NOT NULL
+    AND c.longitude IS NOT NULL
+    AND e.latitude IS NOT NULL
+    AND e.longitude IS NOT NULL
     AND (
         6371 * acos(
-            cos(radians(e.LATITUDE)) *
-            cos(radians(c.LATITUDE)) *
-            cos(radians(c.LONGITUDE) - radians(e.LONGITUDE)) +
-            sin(radians(e.LATITUDE)) *
-            sin(radians(c.LATITUDE))
+            cos(radians(e.latitude)) *
+            cos(radians(c.latitude)) *
+            cos(radians(c.longitude) - radians(e.longitude)) +
+            sin(radians(e.latitude)) *
+            sin(radians(c.latitude))
         )
-    ) <= pn.RAIO_MAXIMO_KM
+    ) <= pn.raio_maximo_km
 """, nativeQuery = true)
     List<PreferenciaNotificacaoModel> buscarUsuariosParaNotificacao(
             @Param("categoria") String categoria,
