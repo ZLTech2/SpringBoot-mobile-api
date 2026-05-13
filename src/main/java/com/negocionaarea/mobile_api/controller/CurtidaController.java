9b8d61ec6c11bcc19df5b488a0a071bae1e7a881
Taynad <166086044+Taynad@users.noreply.github.com>
2026-05-13 20:24:57 -0300

    fix: correção controller com endpoints de curtida

--- src/main/java/com/negocionaarea/mobile_api/controller/CurtidaController.java
+++ src/main/java/com/negocionaarea/mobile_api/controller/CurtidaController.java
@@ -3,10 +3,7 @@ package com.negocionaarea.mobile_api.controller;
 import com.negocionaarea.mobile_api.model.ProdutoModel;
 import com.negocionaarea.mobile_api.service.CurtidaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,11 +19,12 @@ public class CurtidaController {
     }
 
     @PostMapping("/{clienteId}/produto/{produtoId}")
-    public ResponseEntity<String>curtir(@@PathVariable UUID clienteId, @PathVariable UUID produtoId){
+    public ResponseEntity<String>curtir(@PathVariable UUID clienteId, @PathVariable UUID produtoId){
         String mensage = curtidaService.alternarCurtida(clienteId, produtoId);
-        return ResponseEntity.ok(mensage);;
+        return ResponseEntity.ok(mensage);
     }
 
+    @GetMapping("/feed/{clienteId}")
     public ResponseEntity<List<ProdutoModel>>feedCurtidasCliente(@PathVariable UUID clienteId){
         return ResponseEntity.ok(curtidaService.listarPostsCurtidos(clienteId));
     }
